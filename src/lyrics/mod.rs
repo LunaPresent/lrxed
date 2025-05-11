6613dbd2e0b2c622e2d8d69536ace22edefceba2
LunaPresent <github@lunapresent.net>
2025-05-12 00:53:51 +0200

    feat: add highlight currently playing synced line

--- src/lyrics/mod.rs
+++ src/lyrics/mod.rs
@@ -1,8 +1,10 @@
 mod lyric_line;
 mod lyrics;
 mod metadata;
+mod time_index;
 mod timestamp;
 
 pub use lyrics::Lyrics;
-pub use metadata::{Metadata, TagType};
+pub use time_index::{TimeIndex, TimeIndexEntry, TimeIndexHint};
+// pub use metadata::{Metadata, TagType};
 pub use timestamp::Timestamp;
