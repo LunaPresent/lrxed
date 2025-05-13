aa152b6b821959e6cfd05cacbcd8caa5248d4d3d
Luna Present <code@lunapresent.net>
2025-05-14 00:15:16 +0200

    feat: add lrc parser

---
+++ src/lyrics/lyrics.rs
@@ -0,0 +1,95 @@
+use std::{
+	fs::File,
+	io::{BufRead, BufReader},
+};
+
+use color_eyre::eyre;
+
+use super::{lyric_line::LyricLine, metadata::Metadata};
+
+#[derive(Debug, Default, Clone, PartialEq, Eq)]
+pub struct Lyrics {
+	metadata: Vec<Metadata>,
+	lines: Vec<LyricLine>,
+}
+
+impl Lyrics {
+	pub fn from_file(file: File) -> eyre::Result<Self> {
+		let reader = BufReader::new(file);
+		let mut lyrics = Lyrics::default();
+		for line in reader.lines() {
+			lyrics.parse_append(&line?);
+		}
+		Ok(lyrics)
+	}
+
+	pub fn parse_append(&mut self, s: &str) {
+		let lines = s.lines();
+		for line in lines {
+			let mut it = line.trim_start().chars();
+			let first = it.next();
+
+			let (tag_delim, tag_end, _) = match first {
+				Some('[') => it.enumerate().fold(
+					(None, None, false),
+					|(colon_i, close_i, escape), (i, c)| {
+						if close_i.is_some() || escape {
+							(colon_i, close_i, false)
+						} else if colon_i.is_some() {
+							match c {
+								'\\' => (colon_i, None, true),
+								']' => (colon_i, Some(i), false),
+								_ => (colon_i, None, false),
+							}
+						} else {
+							match c {
+								'\\' => (None, None, true),
+								':' => (Some(i), None, false),
+								_ => (None, None, false),
+							}
+						}
+					},
+				),
+				_ => (None, None, false),
+			};
+
+			if let (Some(tag_delim), Some(tag_end)) = (tag_delim, tag_end) {
+				let line = line.trim_start();
+				self.parse_append_line_with_tag(&line[1..tag_end], tag_delim, &line[tag_end + 2..]);
+			} else {
+				self.parse_append_line(line);
+			}
+		}
+	}
+
+	fn parse_append_line_with_tag(&mut self, tag: &str, tag_delim: usize, mut text: &str) {
+		if let Ok(timestamp) = tag.parse() {
+			if text.starts_with(' ') {
+				text = &text[1..];
+			}
+			self.lines
+				.push(LyricLine::new(Some(timestamp), text.to_owned()));
+		} else {
+			self.metadata.push(Metadata::parse_separate(
+				&tag[..tag_delim],
+				&tag[tag_delim + 1..],
+			));
+			text = text.trim();
+			if !text.is_empty() {
+				self.parse_append_line(text);
+			}
+		}
+	}
+
+	fn parse_append_line(&mut self, line: &str) {
+		self.lines.push(LyricLine::new(None, line.to_owned()));
+	}
+
+	pub fn metadata(&self) -> &Vec<Metadata> {
+		&self.metadata
+	}
+
+	pub fn lines(&self) -> &Vec<LyricLine> {
+		&self.lines
+	}
+}
--- src/state/lyrics_state.rs
+++ src/state/lyrics_state.rs
@@ -1,51 +1,8 @@
-use std::time::Duration;
+use crate::lyrics::Lyrics;
 
 #[derive(Debug, Default, Clone, PartialEq, Eq)]
-pub struct LyricsLine {
-	pub time: Option<Duration>,
-	pub time_str: String,
-	pub text: String,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct LyricsState {
-	pub lyrics: Vec<LyricsLine>,
+	pub lyrics: Lyrics,
 	pub cursor_y: usize,
 	pub scroll_y: usize,
-}
-
-impl Default for LyricsState {
-	fn default() -> Self {
-		Self {
-			lyrics: vec![
-				LyricsLine {
-					time: None,
-					time_str: "".to_owned(),
-					text: "If I could".to_owned(),
-				},
-				LyricsLine {
-					time: None,
-					time_str: "".to_owned(),
-					text: "Walk right beside you, show you just who I am".to_owned(),
-				},
-				LyricsLine {
-					time: None,
-					time_str: "".to_owned(),
-					text: "You know I would".to_owned(),
-				},
-				LyricsLine {
-					time: None,
-					time_str: "00:12.87".to_owned(),
-					text: "The memories we made fade".to_owned(),
-				},
-				LyricsLine {
-					time: None,
-					time_str: "00:13.36".to_owned(),
-					text: "I realise we will never be the same".to_owned(),
-				},
-			],
-			cursor_y: 0,
-			scroll_y: 0,
-		}
-	}
 }
