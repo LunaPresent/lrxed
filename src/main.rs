bd5cbe013f4b6c23b829cae915c1459d868635dd
Luna Heyman <luna.heyman@proton.me>
2025-06-03 00:21:36 +0200

    feat: add very basic file browser

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,22 @@ async fn main() -> Result<()> {
 	color_eyre::install()?;
 	let terminal = ratatui::init();
 
-	let mut state: AppState;
-
-	let userdirs = UserDirs::new();
 	let path = if let Some(path) = args.path {
 		path
-	} else if let Some(path) = userdirs.as_ref().and_then(|x| x.audio_dir()) {
+	} else if let Some(path) = UserDirs::new().as_ref().and_then(|x| x.audio_dir()) {
 		path.to_owned()
 	} else {
 		PathBuf::from("/")
 	};
 
+	let mut state: AppState;
+
 	if path.is_file() {
 		state = AppState::new(View::Editor);
+
 		let lrc_path = path.with_extension("lrc");
 		state.audio.audio_player = Some(state.audio.audio_device.try_play(path)?);
+
 		if lrc_path.exists() {
 			state.lyrics.load_file(lrc_path)?;
 		}
--- src/state/file_browser_state.rs
+++ src/state/file_browser_state.rs
@@ -1,7 +1,26 @@
-use std::path::PathBuf;
+use std::{collections::HashMap, fs, path::PathBuf};
 
 #[derive(Default)]
 pub struct FileBrowserState {
+	cache: HashMap<PathBuf, Vec<PathBuf>>,
 	pub directory: PathBuf,
-	pub subdirectories: Vec<PathBuf>,
+	pub selected_line: i16,
+}
+
+impl FileBrowserState {
+	pub fn get_directory_contents(&mut self) -> &[PathBuf] {
+		self.cache
+			.entry(self.directory.clone())
+			.or_insert_with(|| {
+				if let Ok(directory) = fs::read_dir(&self.directory) {
+					directory
+						.filter(Result::is_ok)
+						.map(|r| r.unwrap().path())
+						.collect()
+				} else {
+					vec![]
+				}
+			})
+			.as_slice()
+	}
 }
