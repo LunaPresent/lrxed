5b286cb0d5f95a63be277e0b672082ea40dad6a4
Luna Heyman <luna.heyman@proton.me>
2025-06-12 23:50:26 +0200

    chore: remove unused directory widget

--- src/tui/widgets/mod.rs
+++ src/tui/widgets/mod.rs
@@ -1,10 +1,8 @@
 mod confirm_box;
-mod directory;
 mod lyrics;
 mod playback;
 mod volume;
 
 pub use confirm_box::ConfirmBox;
-pub use directory::DirectoryWidget;
 pub use lyrics::LyricsWidget;
 pub use playback::PlaybackWidget;
