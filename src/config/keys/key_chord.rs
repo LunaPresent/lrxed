0cf31f772c6baaa6e486a9e151339d6f2ecc1446
Luna Present <code@lunapresent.net>
2025-06-12 18:27:44 +0200

    feat: add key config deserialisation

--- src/config/keys/key_chord.rs
+++ src/config/keys/key_chord.rs
@@ -48,6 +48,57 @@ impl FromStr for KeyChord {
 	type Err = eyre::Error;
 
 	fn from_str(s: &str) -> Result<Self, Self::Err> {
-		todo!()
+		match s.chars().count() {
+			0 => Err(eyre::eyre!("Invalid key chord")),
+			1 => Ok(KeyChord::from_char(s.chars().next().unwrap())),
+			_ => {
+				let mut mods = KeyModifiers::NONE;
+				let split_i = s[..s.len() - 1]
+					.rfind('-')
+					.map(|i| i + '-'.len_utf8())
+					.unwrap_or(0);
+				let (mod_str, key_str) = s.split_at(split_i);
+				for m in mod_str.split_terminator('-') {
+					mods.insert(match m {
+						"S" | "s" => KeyModifiers::SHIFT,
+						"C" | "c" => KeyModifiers::CONTROL,
+						"A" | "a" | "M" | "m" => KeyModifiers::ALT,
+						"D" | "d" => KeyModifiers::SUPER,
+						_ => return Err(eyre::eyre!("Invalid modifier identifier")),
+					});
+				}
+				let key = if key_str.chars().count() == 1 {
+					KeyCode::Char(key_str.chars().next().unwrap())
+				} else {
+					match key_str.to_lowercase().as_str() {
+						"backspace" => KeyCode::Backspace,
+						"enter" => KeyCode::Enter,
+						"left" => KeyCode::Left,
+						"right" => KeyCode::Right,
+						"up" => KeyCode::Up,
+						"down" => KeyCode::Down,
+						"home" => KeyCode::Home,
+						"end" => KeyCode::End,
+						"pageup" => KeyCode::PageUp,
+						"pagedown" => KeyCode::PageDown,
+						"tab" => KeyCode::Tab,
+						"backtab" => KeyCode::BackTab,
+						"delete" => KeyCode::Delete,
+						"insert" => KeyCode::Insert,
+						"null" => KeyCode::Null,
+						"esc" => KeyCode::Esc,
+						"capslock" => KeyCode::CapsLock,
+						"scrolllock" => KeyCode::ScrollLock,
+						"numlock" => KeyCode::NumLock,
+						"printscreen" => KeyCode::PrintScreen,
+						"pause" => KeyCode::Pause,
+						"menu" => KeyCode::Menu,
+						"keypadbegin" => KeyCode::KeypadBegin,
+						_ => return Err(eyre::eyre!("Invalid key identifier")),
+					}
+				};
+				Ok(KeyChord::new(key, mods))
+			}
+		}
 	}
 }
