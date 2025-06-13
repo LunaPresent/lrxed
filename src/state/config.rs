82f9edb35b18ac51bf082ce00533779b49c0fb1f
Luna Present <code@lunapresent.net>
2025-06-13 16:54:43 +0200

    feat: add theme config

--- src/state/config.rs
+++ src/state/config.rs
@@ -5,7 +5,6 @@ use crate::config::{KeyMap, Settings, Theme};
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
 #[serde(default)]
 pub struct Config {
-	#[serde(skip)]
 	pub theme: Theme,
 	pub keys: KeyMap,
 	pub settings: Settings,
