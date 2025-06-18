71ac32e4bf52a3fc398137dac60380a91a470cca
Luna Present <code@lunapresent.net>
2025-06-19 00:58:34 +0200

    feat(config): add notification-timeout setting

--- src/config/settings.rs
+++ src/config/settings.rs
@@ -1,6 +1,8 @@
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use serde_with::{DurationSecondsWithFrac, serde_as};
+use std::{path::PathBuf, time::Duration};
 
+#[serde_as]
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "kebab-case")]
 #[serde(default)]
@@ -8,6 +10,8 @@ pub struct Settings {
 	pub scrolloff: u16,
 	pub default_path: Option<PathBuf>,
 	pub replace_txt_file_on_save: bool,
+	#[serde_as(as = "DurationSecondsWithFrac<f64>")]
+	pub notification_timeout: Duration,
 }
 
 impl Default for Settings {
@@ -16,6 +20,7 @@ impl Default for Settings {
 			scrolloff: 8,
 			default_path: None,
 			replace_txt_file_on_save: false,
+			notification_timeout: Duration::from_secs(5),
 		}
 	}
 }
